9c1061c6853572574c870aa0b92329e3733a4a8e
renatog17 <renato.gomes@zup.com.br>
2021-08-20 19:06:19 -0300

    debug testes

--- src/test/kotlin/br/com/zup/edu/registra/CriacaoPixEndpoint.kt
+++ src/test/kotlin/br/com/zup/edu/registra/CriacaoPixEndpoint.kt
@@ -1,16 +1,68 @@
 package br.com.zup.edu.registra
 
+import br.com.zup.edu.CriacaoPixServiceGrpc
+import br.com.zup.edu.DadosCriacaoPixRequest
+import br.com.zup.edu.TipoChave
+import br.com.zup.edu.TipoConta
+import br.com.zup.edu.client.erp.ErpClient
 import br.com.zup.edu.registra.repository.ClienteRepository
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+import org.mockito.Mockito
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import javax.inject.Singleton
 
 @MicronautTest(transactional = false)
 internal class CriacaoPixEndpoint(
-    val repository: ClienteRepository
+    @Inject val repository: ClienteRepository,
+    @Inject val grpcClient: CriacaoPixServiceGrpc.CriacaoPixServiceBlockingStub
 ) {
+
+    @Inject
+    lateinit var itauClient: ErpClient;
 
     @BeforeEach
     fun setUp(){
         repository.deleteAll()
+    }
+
+    @Test
+    fun `deve registrar nova chave pix`(){
+        println("aqui")
+        val response = grpcClient.cadastrar(DadosCriacaoPixRequest.newBuilder()
+            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setTipoChave(TipoChave.EMAIL)
+            .setChave("aaa@aaa.com")
+            .setTipoConta(TipoConta.CONTA_CORRENTE)
+            .build())
+
+        with(response){
+            assertEquals("c56dfef4-7901-44fb-84e2-a2cefb157890", "c56dfef4-7901-44fb-84e2-a2cefb157890")
+            assertNotNull(idPix)
+        }
+    }
+
+//    @MockBean(ErpClient::class)
+//    fun itauClient(): ErpClient? {
+//        return Mockito.mock(ErpClient::class.java)
+//    }
+
+
+    @Factory
+    class Clients{
+        @Singleton
+        fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: ManagedChannel):CriacaoPixServiceGrpc.CriacaoPixServiceBlockingStub?{
+            return CriacaoPixServiceGrpc.newBlockingStub(channel)
+        }
     }
 }
